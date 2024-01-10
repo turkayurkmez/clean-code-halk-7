9d7edb44fe64f732e2e19d880f285210a5b6d273
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-01-10 15:59:19 +0300

    SRP explained

---
+++ SOLID/SingleResponsibility/MailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility
+{
+    public class MailService
+    {
+        public void SendInfoMail(string to)
+        {
+            MessageBox.Show($"{to} adresine mesaj iletildi");
+        }
+    }
+}
