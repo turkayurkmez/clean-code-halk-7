9c3ee94f067ad65bd61bfdf48bd46b5442592160
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-01-10 15:03:50 +0300

    Report generator class updated

--- Functions/Functions/ReportGenerator.cs
+++ Functions/Functions/ReportGenerator.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace Functions
 {
-    internal class ReportGenerator
+    /*
+     * Bir excel dosyası içindeki verileri PDF formatında rapora dönüştürmek istiyorsunuz.
+     * 
+     * Her fonksiyon MÜMKÜN OLDUĞUNCA az satırdan oluşmalıdır 
+     * Gönül ister ki hepsi teksatır olsun.
+     */
+    public class ReportGenerator
     {
+        private string excelFilePath;
+        public ReportGenerator(string path)
+        {
+
+            try
+            {
+                validateFile(path);
+                excelFilePath = path;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        bool isFileExists(string path) => File.Exists(path);
+
+        bool isFormatAvailable(string path)
+        {
+            return true;
+        }
+
+        bool isValidFile(string path)
+        {
+            return isFileExists(path) && isFormatAvailable(path);
+        }
+
+        void validateFile(string path)
+        {
+            if (!isValidFile(path))
+            {
+                throw new Exception();
+            }
+        }
+
     }
 }
